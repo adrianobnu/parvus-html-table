50ce5d0b762b5e3d906a4089f0f029eede4aaaaf
Adriano dos Santos Júnior <adriano@adrianojr.com>
2015-10-23 11:10:54 -0200

    Update HTMLTable.php

--- src/HTMLTable.php
+++ src/HTMLTable.php
@@ -13,6 +13,30 @@
         private $aFooter = array();
         private $hasDataTable = true;
         private $tableClass = 'table table-bordered table-hover table-striped';
+        private $aOrderby = array();
+
+        /**
+         * Define the order of the columns
+         * Ex: array ('col' => 'asc', 'col2' => 'desc')
+         * @param $prArray
+         */
+        public final function orderby ($prArray)
+        {
+
+            foreach ($prArray as $label => $value)
+            {
+
+                if (is_numeric($label))
+                {
+                    $label = $value;
+                    $value = 'asc';
+                }
+
+                $this->aOrderby[$label] = $value;
+
+            }
+
+        }
 
         /**
          * Override the default table class
@@ -97,10 +121,23 @@
 
             $this->htmlTbody .= '<tr '.$this->attribute($prArray).'>';
 
-                foreach ($prArrayItem as $ordem => $value)
+                foreach ($prArrayItem as $ordem => $item)
                 {
 
-                    $this->htmlTbody .= '<td align="'.$this->aHeader[$ordem]['align'].'">'.$value.'</td>';
+                    unset ($orderby,$label);
+
+                    if (is_array($item))
+                    {
+                        $label      = $item['label'];
+                        $orderby    = $item['value'];
+                    }
+                    else
+                    {
+                        $label      = $item;
+                        $orderby    = $item;
+                    }
+
+                    $this->htmlTbody .= '<td align="'.$this->aHeader[$ordem]['align'].'" data-order="'.$orderby.'">'.$label.'</td>';
 
                 }
 
@@ -127,6 +164,11 @@
          */
         public final function html ($prArray = NULL)
         {
+
+            if ($prArray['id'] == NULL)
+            {
+                $prArray['id'] = md5(time()).rand(1,999).uniqid().md5(uniqid());
+            }
 
             $html = '<table class="'.($this->hasDataTable ? 'grid' : NULL).' '.$this->tableClass.'" '.$this->attribute($prArray).'>';
 
@@ -194,7 +236,89 @@
 
             $html .= '</table>';
 
-            return $html;
+
+            $javascript = '$(function() {
+
+                var table = $("#'.$prArray['id'].'").DataTable({
+                    "retrieve"      : true,
+                    "bRetrieve"     : true,
+                    destroy: true,
+                    "responsive"    : true,
+                    "bSortCellsTop" : true,
+                    "columnDefs"    : [ {
+                        "targets"   : "no-sort",
+                        "orderable" : false
+                    }],
+                    language        : {
+                        "sEmptyTable": "Nenhum registro encontrado",
+                        "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                        "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                        "sInfoPostFix": "",
+                        "sInfoThousands": ".",
+                        "sLengthMenu": "Exibindo _MENU_ resultados por página",
+                        "sLoadingRecords": "Carregando...",
+                        "sProcessing": "Processando...",
+                        "sZeroRecords": "Nenhum registro encontrado",
+                        "sSearch": "Pesquisar em todos os campos:",
+                        "oPaginate": {
+                            "sNext": "Próximo",
+                            "sPrevious": "Anterior",
+                            "sFirst": "Primeiro",
+                            "sLast": "Último"
+                        },
+                        "oAria": {
+                            "sSortAscending": ": Ordenar colunas de forma ascendente",
+                            "sSortDescending": ": Ordenar colunas de forma descendente"
+                        }
+                    }
+                });
+
+                $("#'.$prArray['id'].'").find("input[type=text],select").on("keyup change", function ()
+                {
+
+                    table.column($(this).attr("ordem")).search(this.value).draw();
+
+                });';
+
+                /** Se tem ordenação **/
+                if ($this->aOrderby != NULL)
+                {
+
+                    $javascript .= chr(10).'table.order(';
+
+                        $orderFilter = 1;
+
+                        /** Varre os campos **/
+                        foreach ($this->aHeader as $order => $item)
+                        {
+
+                            if ($this->aOrderby[$item['label']] != NULL)
+                            {
+
+                                if ($orderFilter > 1)
+                                {
+                                    $javascript .= ',';
+                                }
+
+                                $javascript .= '['.$order.', "'.$this->aOrderby[$item['label']].'"]';
+
+                                $orderFilter ++;
+
+                            }
+
+                        }
+
+                    $javascript.= ').draw();';
+
+                }
+
+            $javascript .= '});';
+
+            return array (
+                'html'          => $html,
+                'javascript'    => $javascript
+            );
 
         }
 
